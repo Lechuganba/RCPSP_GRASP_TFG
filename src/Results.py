d3d43136dccfa94b23feae8ea6889ba408252304
Lechuganba <48476036+Lechuganba@users.noreply.github.com>
2022-06-21 23:07:50 +0200

    21/6/2022

--- src/Results.py
+++ src/Results.py
@@ -1,22 +1,22 @@
 import pandas as pd
 
 
-def processResults():
-    j30 = pd.read_csv("../results/j30.csv")
-    j60 = pd.read_csv("../results/j60.csv")
-    j90 = pd.read_csv("../results/j90.csv")
-    j120 = pd.read_csv("../results/j120.csv")
+def processResults(alphas, dir):
+    if dir == "j30":
+        j30 = pd.read_csv("../results/j30.csv")
+        compareOpt("j30", j30, alphas)
+    elif dir == "j60":
+        j60 = pd.read_csv("../results/j60.csv")
+        compareOpt("j60", j60, alphas)
+    elif dir == "j90":
+        j90 = pd.read_csv("../results/j90.csv")
+        compareOpt("j90", j90, alphas)
+    elif dir == "j120":
+        j120 = pd.read_csv("../results/j120.csv")
+        compareOpt("j120", j120, alphas)
 
-    compareOpt("j30", j30)
-    compareOpt("j60", j60)
-    compareOpt("j90", j90)
-    compareOpt("j120", j120)
 
-
-def compareOpt(problemType, panda):
-    draw = 0
-    worse = 0
-    better = 0
+def compareOpt(problemType, panda, alphas):
     outputPath = "../results/compare" + problemType + ".csv"
     outputFile = open(outputPath, "w")
     if problemType == "j30":
@@ -27,20 +27,25 @@ def compareOpt(problemType, panda):
         start = [5, "\t"]
     inputFile = open(inputPath, "r")
     lines = inputFile.readlines()
-    outputFile.write("problemType, GRASP, OPT/HEUR, DRAW\n")
-    for result in panda.values:
-        name = result[0]
-        opts = findLine(lines, name, start, problemType)
-        if opts[2] == result[2]:
-            draw = draw + 1
-        elif opts[2] > result[2]:
-            better = better + 1
-        else:
-            worse = worse + 1
-    outputFile.write(problemType + ", " + str(better) + ", " + str(worse) + ", " + str(worse))
+    outputFile.write("problemType, alpha, GRASP, OPT/HEUR, DRAW\n")
+    for alpha in alphas:
+        draw = 0
+        worse = 0
+        better = 0
+        for result in panda.values:
+            if alpha == result[1]:
+                name = result[0]
+                opts = findLines(lines, name, start, problemType, alpha)
+                if opts[2] == result[2]:
+                    draw = draw + 1
+                elif opts[2] > result[2]:
+                    better = better + 1
+                else:
+                    worse = worse + 1
+        outputFile.write(problemType + ", " + str(alpha) + ", " + str(better) + ", " + str(worse) + ", " + str(draw) + "\n")
 
 
-def findLine(lines, name, start, problemType):
+def findLines(lines, name, start, problemType, alpha):
     nameAux = name.split("_")
     i = nameAux[0][3:len(nameAux[0])]
     j = nameAux[1]
--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--problem', type=str, default="/j120.sm/", help='Path al proyecto que se quiere ejecutar, desde ../resources')
     parser.add_argument(
-        '--iterations', type=str, default="25",
+        '--iterations', type=str, default="50",
         help='Número de iteraciones a ejecutar por el algoritmo, debe ser mayor que 0')
     parser.add_argument(
         '--alpha', type=str, default="0.5", help='Valor de la variable alpha, debe ser entre 0 y 1')
@@ -24,29 +24,32 @@ if __name__ == '__main__':
     # y creamos el proyecto
     dirs = ["j30", "j60", "j90", "j120"]
     for dir in dirs:
-        alpha = round(random.random(), 2)
+        alphas = []
+        for i in range(0, 4):
+            alphaAux = round(random.random(), 2)
+            alphas.append(alphaAux)
         folder = "/" + dir + ".sm/"
         listdir = os.listdir("../resources/" + dir + ".sm")
-        instances = random.sample(listdir, 50)
+        instances = random.sample(listdir, 507)
         path = "../results/" + dir + ".csv"
         file = open(path, "w")
         file.write("projectName, alpha, makespan, duration;\n")
-        for j in instances:
-            filepath = "../resources" + folder + j
-            fp = open(filepath, "r")
-            # Creación del proyecto
-            projectName = j
-            projectNameAux = projectName.split(".")[0]
-            project = InputData.readFile(projectName, dir, fp)
-            # Número de iteraciones
-            maxIterations = int(args.iterations)
-            initMS = time.time()
-            bestSol = GRASP.startAlgorithm(maxIterations, project, alpha)
-            # Finalizamos y mostramos el mejor tiempo
-            finishMS = time.time()
-            bestSol.duration = finishMS - initMS
-            file.write(projectNameAux + "," + str(alpha) + "," + str(bestSol.makespan) + "," + str(bestSol.duration) + ";\n")
+        for alpha in alphas:
+            for j in instances:
+                filepath = "../resources" + folder + j
+                fp = open(filepath, "r")
+                # Creación del proyecto
+                projectName = j
+                projectNameAux = projectName.split(".")[0]
+                project = InputData.readFile(projectName, dir, fp)
+                # Número de iteraciones
+                maxIterations = int(args.iterations)
+                initMS = time.time()
+                bestSol = GRASP.startAlgorithm(maxIterations, project, alpha)
+                # Finalizamos y mostramos el mejor tiempo
+                finishMS = time.time()
+                bestSol.duration = finishMS - initMS
+                file.write(projectNameAux + "," + str(alpha) + "," + str(bestSol.makespan) + "," + str(bestSol.duration) + ";\n")
         file.close()
-
-    Results.processResults()
+        Results.processResults(alphas, dir)
 
